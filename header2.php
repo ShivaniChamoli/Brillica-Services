d1007f471427fcf2988ea37f25c245cf9c1b88d0
Shivani Chamoli <shivaniichamoli@gmail.com>
2021-06-08 16:43:11 +0530

    update

--- header2.php
+++ header2.php
@@ -13,6 +13,30 @@
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
+
+<div class="container-fluid" style="background-color:#242424">
+
+<div class="row">
+<div class="col-lg-4 col-xl-4 col-md-4 .col-4 col-sm-4">
+<a  href="tel:88821-40688"><i class="fas fa-phone-alt"></i><span class="txt-con"> +91 88821-40688  &nbsp|</a></span> <a  href="tel:90840-63259"><span class="txt-con2"> +91 90840-63259</a></span>
+       
+</div>
+<div class="col-lg-2 col-xl-2 col-md-2 .col-2 col-sm-2">
+<a href="mailto:info@brillicaservices.com" target="_blank" > <i class="fas fa-envelope twitterx2"></i></a>
+              </div>
+<div class="col-lg-6 col-xl-5 .col-6 col-sm-6 lll">
+        
+
+        <a href="https://www.facebook.com/BrillicaServices"><i class="fab fa-facebook"></i></a>
+        <a href="https://www.linkedin.com/company/brillica-services-pvt-ltd" target="_blank" > <i class="fab fa-linkedin-in twitter1"></i></a>
+              <a href="https://twitter.com/BrillicaServic1" target="_blank" > <i class="fab fa-twitter twitter1"></i></a>
+              <a href="https://www.instagram.com/brillicaservices0309/"><i class="fab fa-instagram twitter1"></i></a>    
+</div>
+
+
+
+</div>
+</div>
     <div class="container-fluid">
 <div class="row">
 <div class="col-lg-3 col-xl-3 col-md-3 .col-3">
@@ -22,18 +46,7 @@
     class="BRILLICA-HD-Logo"></a>
 </div>
 
-    <div class="col-lg-7 col-xl-7 .col-7">
-        <div class="black-box">
-
-        <a  href="tel:88821-40688"><i class="fas fa-phone-alt"></i><span class="txt-con"> +91 88821-40688  &nbsp|</a></span> <a  href="tel:90840-63259"><span class="txt-con2"> +91 90840-63259</a></span>
-       <a href="https://www.facebook.com/BrillicaServices"><i class="fab fa-facebook"></i></a>
-              <a href="https://www.linkedin.com/company/brillica-services-pvt-ltd" target="_blank" > <i class="fab fa-linkedin-in twitter1"></i></a>
-              <a href="https://twitter.com/BrillicaServic1" target="_blank" > <i class="fab fa-twitter twitter1"></i></a>
-              <a href="https://www.instagram.com/brillicaservices0309/"><i class="fab fa-instagram twitter1"></i></a>
-              
-        
-    </div>      
-</div>
+  
 <div class="col-lg-2 col-xl-2"></div>
 </div>
 
