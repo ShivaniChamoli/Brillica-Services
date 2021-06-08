271fbd020fea00346fd08821b51b51c33c3176c9
Shivani Chamoli <shivaniichamoli@gmail.com>
2021-06-08 13:57:35 +0530

    update

--- header2.php
+++ header2.php
@@ -15,27 +15,26 @@
 <body>
     <div class="container-fluid">
 <div class="row">
-<div class="col-lg-3 col-xlg-3">
+<div class="col-lg-3 col-xl-3 col-md-3 .col-3">
   <a href="./index.php"  ><img src="img/BRILLICA HD Logo.png"
     srcset="img/BRILLICA HD Logo@2x.png 2x,
             img/BRILLICA HD Logo@3x.png 3x"
     class="BRILLICA-HD-Logo"></a>
 </div>
 
-    <div class="col-lg-9 col-xl-9" style="background-color:#242424">
+    <div class="col-lg-7 col-xl-7 .col-7">
         <div class="black-box">
 
         <a  href="tel:88821-40688"><i class="fas fa-phone-alt"></i><span class="txt-con"> +91 88821-40688  &nbsp|</a></span> <a  href="tel:90840-63259"><span class="txt-con2"> +91 90840-63259</a></span>
-            <a href="https://www.facebook.com/BrillicaServices"><i class="fab fa-facebook"></i></a>
+       <a href="https://www.facebook.com/BrillicaServices"><i class="fab fa-facebook"></i></a>
               <a href="https://www.linkedin.com/company/brillica-services-pvt-ltd" target="_blank" > <i class="fab fa-linkedin-in twitter1"></i></a>
               <a href="https://twitter.com/BrillicaServic1" target="_blank" > <i class="fab fa-twitter twitter1"></i></a>
               <a href="https://www.instagram.com/brillicaservices0309/"><i class="fab fa-instagram twitter1"></i></a>
               
         
-    </div>
-   
-        
+    </div>      
 </div>
+<div class="col-lg-2 col-xl-2"></div>
 </div>
 
 
